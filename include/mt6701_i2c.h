8c330114ef0488d7ba43b8778071704f6ca8f073
Enix Yu <enix223@163.com>
2024-10-15 10:01:29 +0800

    fix: various syntax error

--- include/mt6701_i2c.h
+++ include/mt6701_i2c.h
@@ -1,53 +1,54 @@
 #pragma once
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
 #include "driver/i2c_master.h"
 
-  struct esp_mt6701_dev_t
-  {
-    i2c_master_dev_handle_t *dev_handle;
-  };
+#define ESP_MT6701_I2C_ADDRESS_DEFAULT (0x6)
 
-  typedef struct esp_mt6701_dev_t *esp_mt6701_dev_handle_t;
+struct esp_mt6701_dev_t {
+  i2c_master_dev_handle_t dev_handle;
+};
 
-  /**
-   * @brief Create and init sensor object and return a sensor handle
-   *
-   * @param[in] dev_addr I2C device address of sensor, if equals -1, then default address is used
-   *
-   * @return
-   *     - NULL Fail
-   *     - Others Success
-   */
-  esp_mt6701_dev_handle_t esp_mt6701_create(uint16_t dev_addr);
+typedef struct esp_mt6701_dev_t *esp_mt6701_dev_handle_t;
 
-  /**
-   * @brief Delete and release a sensor object
-   *
-   * @param handle object handle of mt6701
-   *
-   * @return
-   *     - ESP_OK Success
-   *     - ESP_FAIL Fail
-   */
-  esp_err_t esp_mt6701_delete(esp_mt6701_dev_handle_t handle);
+/**
+ * @brief Create and init sensor object and return a sensor handle
+ *
+ * @param[in] dev_addr I2C device address of sensor, if equals -1, then default address is used
+ * @param[out] handle MT6701 dev handle
+ *
+ * @return
+ *     - NULL Fail
+ *     - Others Success
+ */
+esp_err_t esp_mt6701_create(uint16_t dev_addr, esp_mt6701_dev_handle_t *handle);
 
-  /**
-   * @brief Get angle from mt6701
-   *
-   * @param      handle object handle of mt6701
-   * @param[out] angle angle value got from mt6701 in [int16_t]
-   * @param[out] angle_f angle value got from mt6701 in [float]
-   *
-   * @return
-   *     - ESP_OK Success
-   *     - ESP_FAIL Fail
-   */
-  esp_err_t esp_mt6701_get_angle(esp_mt6701_dev_handle_t handle, int16_t *const angle, float *const angle_f);
+/**
+ * @brief Delete and release a sensor object
+ *
+ * @param handle object handle of mt6701
+ *
+ * @return
+ *     - ESP_OK Success
+ *     - ESP_FAIL Fail
+ */
+esp_err_t esp_mt6701_delete(esp_mt6701_dev_handle_t handle);
+
+/**
+ * @brief Get angle from mt6701
+ *
+ * @param      handle object handle of mt6701
+ * @param[out] angle angle value got from mt6701 in [int16_t]
+ * @param[out] angle_f angle value got from mt6701 in [float]
+ *
+ * @return
+ *     - ESP_OK Success
+ *     - ESP_FAIL Fail
+ */
+esp_err_t esp_mt6701_get_angle(esp_mt6701_dev_handle_t handle, int16_t *const angle, float *const angle_f);
 
 #ifdef __cplusplus
 }
--- mt6701_i2c.c
+++ mt6701_i2c.c
@@ -1,16 +1,17 @@
-#include <stdlib.h>
 #include "mt6701_i2c.h"
-#include "driver/i2c_master.h"
 
-#define MT6701_I2C_ADDRESS_DEFAULT (0x6)
+#include <stdlib.h>
+
+#include "driver/i2c_master.h"
+#include "esp_log.h"
+
 #define MT6701_REG_ANGLE_14B 0x03
 #define MT6701_READ_TIMEOUT 50
 
 static char *TAG = "mt6701";
 static i2c_master_bus_handle_t i2c_master_bus_handle;
 
-static esp_err_t esp_mt6701_init()
-{
+static esp_err_t esp_mt6701_init() {
   i2c_master_bus_config_t i2c_mst_config = {
       .i2c_port = CONFIG_ESP_MT6701_I2C_PORT_NUM,
       .sda_io_num = CONFIG_ESP_MT6701_I2C_SDA_PIN_NUM,
@@ -20,13 +21,12 @@ static esp_err_t esp_mt6701_init()
   };
 
   ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_mst_config, &i2c_master_bus_handle));
-  ESP_LOGI(TAG, "Initialize i2c master bus success");
+  ESP_LOGD(TAG, "Initialize i2c master bus success");
 
   return ESP_OK;
 }
 
-esp_err_t esp_mt6701_get_angle(esp_mt6701_dev_handle_t handle, int16_t *const angle, float *const angle_f)
-{
+esp_err_t esp_mt6701_get_angle(esp_mt6701_dev_handle_t handle, int16_t *const angle, float *const angle_f) {
   esp_err_t ret = ESP_OK;
   uint8_t data[2] = {0, 0};
 
@@ -38,31 +38,34 @@ esp_err_t esp_mt6701_get_angle(esp_mt6701_dev_handle_t handle, int16_t *const an
   return ret;
 }
 
-esp_mt6701_dev_handle_t esp_mt6701_create(uint16_t dev_addr)
-{
-  if (i2c_master_bus_handle == NULL)
-  {
-    ESP_ERROR_CHECK(esp_mt6701_init());
+esp_err_t esp_mt6701_create(uint16_t dev_addr, esp_mt6701_dev_handle_t *handle) {
+  esp_err_t err;
+  if (i2c_master_bus_handle == NULL) {
+    err = esp_mt6701_init();
+    if (err != ESP_OK) {
+      return err;
+    }
   }
 
-  esp_mt6701_dev_handle_t handle = (esp_mt6701_dev_handle_t)calloc(1, sizeof(struct esp_mt6701_dev_t));
+  esp_mt6701_dev_handle_t mt6701_handle = (esp_mt6701_dev_handle_t)calloc(1, sizeof(struct esp_mt6701_dev_t));
 
-  ESP_LOGI(TAG, "Register mt6701 i2c device@%d", dev_addr);
+  ESP_LOGD(TAG, "Register mt6701 i2c device@%d", dev_addr);
   i2c_device_config_t dev_cfg = {
       .dev_addr_length = I2C_ADDR_BIT_LEN_7,
-      .device_address = dev_addr == -1 ? MT6701_I2C_ADDRESS_DEFAULT : dev_addr,
+      .device_address = dev_addr,
       .scl_speed_hz = CONFIG_ESP_MT6701_I2C_SCL_SPEED,
   };
   i2c_master_dev_handle_t i2c_magnetic_dev_handle;
   ESP_ERROR_CHECK(i2c_master_bus_add_device(i2c_master_bus_handle, &dev_cfg, &i2c_magnetic_dev_handle));
 
-  handle->dev_handle = i2c_magnetic_dev_handle;
-  ESP_LOGI(TAG, "Register knob device success");
-  return handle;
+  mt6701_handle->dev_handle = i2c_magnetic_dev_handle;
+  *handle = mt6701_handle;
+  ESP_LOGD(TAG, "Register knob device success");
+
+  return ESP_OK;
 }
 
-esp_err_t esp_mt6701_delete(esp_mt6701_dev_handle_t handle)
-{
+esp_err_t esp_mt6701_delete(esp_mt6701_dev_handle_t handle) {
   i2c_master_bus_rm_device(handle->dev_handle);
   free(handle);
   return ESP_OK;
